9371e101edcac5b7edd65d64c473eb02bcd8d2cd
sbabaiev <serhii@nexar.app>
2024-07-01 13:05:30 +0300

    Add slideshow and home page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 import MainHeader from "@/components/main-header/main-header";
-import MainHeaderBackground from "@/components/main-header/main-header-background";
 
 export const metadata = {
   title: "NextLevel Food",
